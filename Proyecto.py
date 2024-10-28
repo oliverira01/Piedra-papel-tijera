5588063eed24a6597347f655075175783ac1a11f
Oliveira <oliveirita01@gmail.com>
2024-10-28 15:22:21 +0100

    Numero pc y empezar el casos victoria etc

--- Proyecto.py
+++ Proyecto.py
@@ -1,13 +1,23 @@
+import random
+
 print("Vamos a jugar Piedra, Papel, Tijera")
-eleccion = input("Elije que quieres usar (1-->Piedra  2-->Papel  3-->Tijera):")
+eleccion = int(input("Elije que quieres usar (0-->Piedra  1-->Papel  2-->Tijera):"))
 match eleccion:
-        case "1":
+        case 0:
             print("Piedra")
-        case "2":
+        case 1:
             print("Papel")
-        case "3":
+        case 2:
             print("Tijera")
         case _:
             print("Accion no valida")
 
+def ordenador():
+    (random.randint(0, 2))
 
+def quien_gana():
+    if eleccion == ordenador():
+        print("EMPATE")
+    elif eleccion == 0 and ordenador() == 1:
+        print("Gana Ordenador")
+    
